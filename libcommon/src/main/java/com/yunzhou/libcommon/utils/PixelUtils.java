08cfa108e5b309f9f80177184ac306f432a5563d
华云舟 <huayunzhou@fclassroom.com>
2017-09-14 16:30:57 +0800

    1.添加Pixel，Screen两个工具库

---
+++ libcommon/src/main/java/com/yunzhou/libcommon/utils/PixelUtils.java
@@ -0,0 +1,52 @@
+package com.yunzhou.libcommon.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+/**
+ * 像素单位相关，用于dp，sp，px之间的相互转换
+ * Created by huayunzhou on 2017/9/14.
+ */
+
+@SuppressWarnings("unused")
+public class PixelUtils {
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     */
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     */
+    public static int dip2px(float dipValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+}
---
+++ libcommon/src/main/java/com/yunzhou/libcommon/utils/ScreenUtils.java
@@ -0,0 +1,61 @@
+package com.yunzhou.libcommon.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+/**
+ * 屏幕尺寸相关，获取屏幕宽度，高度等等
+ * Created by huayunzhou on 2017/9/14.
+ */
+
+@SuppressWarnings("unused")
+public class ScreenUtils {
+
+    /**
+     * 获取屏幕的宽和高
+     */
+    public static int[] getScreenSize() {
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        return new int[]{displayMetrics.widthPixels, displayMetrics.heightPixels};
+    }
+
+    /**
+     * 获取屏幕宽度
+     */
+    public static int getScreenWidth(){
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        return displayMetrics.widthPixels;
+    }
+
+    /**
+     * 获取屏幕高度
+     */
+    public static int getScreenHeight(){
+        DisplayMetrics displayMetrics = Resources.getSystem().getDisplayMetrics();
+        return displayMetrics.heightPixels;
+    }
+
+
+    /**
+     * 获取状态栏高度
+     */
+    public static int getStatusBarHeight(Context context){
+        int statusBarHeight = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+
+
+    /**
+     * 获取除去状态栏高度的屏幕高度
+     */
+    public static int getScreenHeightWithoutStatusbar(Context context){
+        return getScreenHeight() - getStatusBarHeight(context);
+    }
+
+}
