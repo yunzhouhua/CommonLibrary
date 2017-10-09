23a5def1202080609f35af0c046030b28d3cc35d
华云舟 <yunzhou.hua@gmail.com>
2017-10-09 14:50:17 +0800

    网络请求添加日志输出

--- libcommon/src/main/java/com/yunzhou/libcommon/net/http/log/NetLog.java
+++ libcommon/src/main/java/com/yunzhou/libcommon/net/http/log/NetLog.java
@@ -1,6 +1,5 @@
 package com.yunzhou.libcommon.net.http.log;
 
-import android.util.ArrayMap;
 import android.util.Log;
 
 import com.yunzhou.libcommon.net.http.HttpError;
@@ -15,31 +14,31 @@ public class NetLog {
     private static final String TAG = "HTTP";
 
     public static void start(Request request){
-        Log.e(TAG, "==================== START ====================");
-        Log.e(TAG, "== url : " + request.getUrl());
+        Log.d(TAG, "==================== START ====================");
+        Log.d(TAG, "== url : " + request.getUrl());
         if(request.getParams() != null && request.getParams().size() > 0) {
-            Log.e(TAG, "== params : " + request.getParams().toString());
+            Log.d(TAG, "== params : " + request.getParams().toString());
         }
     }
 
     public static void endFail(String url, HttpError error){
-        Log.e(TAG, "==================== END FAIL ====================");
-        Log.e(TAG, "== url : " + url);
-        Log.e(TAG, "== error code : " + error.getCode());
-        Log.e(TAG, "== error msg : " + error.getMessage());
+        Log.d(TAG, "==================== END FAIL ====================");
+        Log.d(TAG, "== url : " + url);
+        Log.d(TAG, "== error code : " + error.getCode());
+        Log.d(TAG, "== error msg : " + error.getMessage());
     }
 
     public static void endSuccess(String url, Object result){
-        Log.e(TAG, "==================== END SUCCESS ====================");
-        Log.e(TAG, "== url : " + url);
+        Log.d(TAG, "==================== END SUCCESS ====================");
+        Log.d(TAG, "== url : " + url);
         if(result == null){
-            Log.e(TAG, "== result : null");
+            Log.d(TAG, "== result : null");
         }else if(result instanceof String){
-            Log.e(TAG, "== result : " + (String)result);
+            Log.d(TAG, "== result : " + (String)result);
         }else if(result instanceof byte[]){
-            Log.e(TAG, "== result : " + new String((byte[])result));
+            Log.d(TAG, "== result : " + new String((byte[])result));
         }else{
-            Log.e(TAG, "== result : " + result.toString());
+            Log.d(TAG, "== result : " + result.toString());
         }
     }
 }
